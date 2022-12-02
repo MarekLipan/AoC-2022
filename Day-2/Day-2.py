739d7d49bab16bd715a3c629e9910e39ff759540
Marek Lipan <marek.lipan@datasentics.com>
2022-12-02 10:27:35 +0100

    draft 2

--- Day-2/Day-2.py
+++ Day-2/Day-2.py
@@ -30,7 +30,32 @@ def total_score(l: list) -> int:
     return total_score
 
 
-print(total_score(L))
+def total_score_2(l: list) -> int:
+    """
+    Total score obtained for rock-paper-scissors game.
+    """
+
+    total_score = 0
+
+    opponent_moves = ["A", "B", "C"]
+    results = ["X", "Y", "Z"]
+
+    for i in l:
+        opponent=opponent_moves.index(i[0])
+        result=results.index(i[2]) * 3
+
+        if result==3:
+            # draw
+            total_score += result + opponent_moves
+        elif result==6:
+            # win
+            total_score += result + (opponent + 1) % 3 + 1
+        elif result==0:
+            total_score += result + (opponent - 1) % 3 + 1
+
+    return total_score
+
+print(total_score_2(L))
 
 
 
